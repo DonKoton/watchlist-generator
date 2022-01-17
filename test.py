b06ee10313c2096cb492be4e60fce04b8a338d28
Michal <m.kotecki96@o2.pl>
2022-01-17 22:26:48 +0100

    people_db repair and select update

--- test.py
+++ test.py
@@ -4,16 +4,32 @@ from pprint import pprint
 
 connection = sqlite3.connect("data.db")
 
-get_movies_stmt = "SELECT * FROM movies INNER JOIN ratings ON ratings.tconst = movies.tconst WHERE averageRating > 7.0 AND genres LIKE ? AND movies.startYear > 2000 AND ratings.numVotes > 20000"
+get_movies_stmt = """
+SELECT movies.primaryTitle FROM movies
+INNER JOIN ratings ON ratings.tconst = movies.tconst
+INNER JOIN roles ON roles.tconst = movies.tconst
+INNER JOIN crew ON crew.tconst = movies.tconst
+INNER JOIN people ON people.nconst = roles.nconst
+WHERE averageRating > ?
+AND genres LIKE ?
+AND movies.startYear > ?
+AND ratings.numVotes > ?
+AND roles.category LIKE ?
+AND people.primaryName = ?
+AND people.birthYear = ?"""
 
 
-def genre(kword):
-    kword = '%' + kword + '%'
+def get_movies(rating: float, genre: str, prod_year: int, votes: int, role: str, name: str, birth_year):
+    genre = '%' + genre + '%'
+    role = '%' + role + '%'
 
     with connection:
-        result = connection.execute(get_movies_stmt, (kword,))
+        result = connection.execute(get_movies_stmt, (rating, genre, prod_year, votes, role, name, birth_year))
     return result.fetchall()
 
 
-list_of_movies = genre('war')
-pprint(sample(list_of_movies, k=7))
+list_of_movies = get_movies(6.0, '', 1970, 5000, 'actor', 'Dwayne Johnson', 1972)
+try:
+    pprint(sample(list_of_movies, k=5))
+except ValueError:
+    print("Not enough movies matching selected filters!")
