32c6a8b4f5db4f67899555ab23c225e9b93a1976
Uzytkownik <uzytkownik@email.com>
2022-01-18 21:15:58 +0100

    odhaczenie opcji Amend

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,2 +1,20 @@
 from django.db import models
+from database import create_choices
+
+
+class Movies(models.Model):
+    CHOICES_GENRES = create_choices()
+    CHOICES_ROLES = [
+        ("actor", "Actor"),
+        ("actress", "Actress"),
+        ("director", "Director")
+    ]                                       # fill in the collection of roles
+
+    rating = models.FloatField(min_value=1, max_value=10)
+    votes = models.IntegerField(max_length=10)
+    genre = models.CharField(choices=CHOICES_GENRES)
+    prod_year = models.IntegerField(max_length=4)
+    role = models.CharField(choices=CHOICES_ROLES)
+    name = models.CharField(max_length=128)
+    birth_year = models.IntegerField(max_length=4)
 
