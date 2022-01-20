9b515ba63670b2c90a7e016b00a7ddffb05e0671
Uzytkownik <uzytkownik@email.com>
2022-01-20 18:37:47 +0100

    added validators into separate file and filled in with it model's fields

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from database import create_choices
-from validators import MaxValueValidator, MinValueValidator, MinLengthValidator, MaxLengthValidator
+from watchlist.validators import MaxValueValidator, MinValueValidator, MinLengthValidator, MaxLengthValidator
 
 class Movies(models.Model):
     CHOICES_GENRES = create_choices()
@@ -10,11 +10,11 @@ class Movies(models.Model):
         ("director", "Director")
     ]
 
-    rating = models.FloatField()
-    votes = models.IntegerField()
-    genre = models.CharField(choices=CHOICES_GENRES)
-    prod_year = models.IntegerField()
-    role = models.CharField(choices=CHOICES_ROLES)
+    rating = models.FloatField(validators=[MinValueValidator(1.0), MaxValueValidator(10.0)])
+    votes = models.IntegerField(validators=[MinValueValidator(1000), MaxLengthValidator()])  # TODO insert value as max argument from database
+    genre = models.CharField(max_length=32, choices=CHOICES_GENRES)
+    prod_year = models.IntegerField(validators=[MinValueValidator(1960), MaxValueValidator()]) # TODO insert datetime into func arg
+    role = models.CharField(max_length=16, choices=CHOICES_ROLES)
     name = models.CharField(max_length=128)
-    birth_year = models.IntegerField()
+    birth_year = models.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator()]) # TODO insert datetime into func arg
 
